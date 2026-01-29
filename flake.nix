ca335d7b049a45e9f0ec1531757774418f8537c1
VijetHegde604 <vijethegde604@gmail.com>
2026-01-30 00:46:20 +0530

    Added plymouth

--- flake.nix
+++ flake.nix
@@ -1,22 +1,32 @@
 {
-  description = "My NixOS config";
+  description = "Vijet's NixOS config";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixos-boot = {
+      url = "github:Melkor333/nixos-boot";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, nixos-boot, ... }: {
     nixosConfigurations.nix-btw =
       nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+
         specialArgs = { inherit inputs; };
 
         modules = [
+          nixos-boot.nixosModules.default
+
           ./configuration.nix
+
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
