359d0975da8afd2ea4dc8430919cf6ddc4f133b7
VijetHegde604 <vijethegde604@gmail.com>
2026-01-29 16:01:22 +0530

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,109 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  # --- Bootloader & Kernel ---
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # --- Networking ---
+  networking.hostName = "nix-btw";
+  networking.networkmanager.enable = true;
+  services.resolved.enable = true;
+  services.tailscale.enable = true;
+
+  # --- Localization ---
+  time.timeZone = "Asia/Kolkata";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # --- Window Manager & Desktop Services ---
+  services.xserver.enable = true;
+  services.displayManager.gdm.enable = true;      
+  
+  programs.niri.enable = true;
+  programs.dms-shell.enable = true; # Dank Material Shell
+
+  # XDG Portals Configuration
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+    configPackages = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+    xdgOpenUsePortal = true;
+  };
+
+  # --- Hardware & Sound ---
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+  
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.libinput.enable = true;
+  services.upower.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.acpid.enable = true;
+
+  systemd.services.battery-threshold = {
+  description = "Set battery charge threshold";
+  after = [ "multi-user.target" ];
+  wantedBy = [ "multi-user.target" ];
+  serviceConfig = {
+    Type = "oneshot";
+    ExecStart = "${pkgs.bash}/bin/bash -c 'echo 80 > /sys/class/power_supply/BAT0/charge_control_end_threshold'";
+    RemainAfterExit = true;
+  };
+};
+
+  # --- User Account ---
+  users.users.vijeth = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" ];
+    packages = with pkgs; [ tree ];
+  };  
+
+  # --- System Packages ---
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  environment.systemPackages = with pkgs; [
+    # Terminal & CLI Tools
+    vim nano fastfetch ghostty btop mise starship 
+    zoxide lsd git wtype bash-completion lazygit
+    libsecret ripgrep fd bat 
+    
+    # GUI Apps
+    brave nautilus 
+    seahorse zed-editor vscode 
+    
+    # Icon Themes 
+    papirus-icon-theme
+    adwaita-icon-theme
+    xdg-user-dirs
+  ];
+
+  # --- Fonts ---
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+  ];
+
+  # --- Misc Services ---
+  services.gnome.gnome-keyring.enable = true;
+  programs.firefox.enable = true;
+
+
+  system.stateVersion = "25.11"; 
+}
