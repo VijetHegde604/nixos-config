babd68b86ab8e3dde9860424d9433eafabaa4a9c
VijetHegde604 <vijethegde604@gmail.com>
2026-01-29 21:57:59 +0530

    Added Custom DNS

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
   # --- Networking ---
   networking.hostName = "nix-btw";
   networking.networkmanager.enable = true;
+  networking.nameservers = [ "100.95.4.126" "1.1.1.1" ];
   services.resolved.enable = true;
   services.tailscale.enable = true;
 
