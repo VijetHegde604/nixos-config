b8c5309026c286751d3b11509c4bebfc8ff56bd9
VijetHegde604 <vijethegde604@gmail.com>
2026-01-29 23:09:38 +0530

    Updated and tidied flake.nix

--- flake.nix
+++ flake.nix
@@ -1,33 +1,29 @@
 {
-  description = "A very basic flake";
+  description = "My NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let 
-      system = "x86_64-linux";
-    in
-    {
-      nixosConfigurations.nix-btw = nixpkgs.lib.nixosSystem {
-        inherit system;
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nix-btw =
+      nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         specialArgs = { inherit inputs; };
+
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            # This allows you to use 'inputs' inside your home-manager config
             home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
       };
-    };
+  };
 }
