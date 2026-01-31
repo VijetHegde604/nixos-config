031d2ef16cae945254ae663dc28999d95aecd5cf
VijetHegde604 <vijethegde604@gmail.com>
2026-01-31 13:12:35 +0530

    refactor: simplify nfu alias to only update flake inputs

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -22,7 +22,7 @@
       nrt = "sudo nixos-rebuild test --flake ~/nixos-config#nix-btw";
 
       # update flake inputs then rebuild
-      nfu = "nix flake update && sudo nixos-rebuild switch --flake ~/nixos-config#nix-btw";
+      nfu = "nix flake update";
 
       # nix store cleanup helpers
       nixgc = "sudo nix-collect-garbage -d";
