163eb6b5fe879ca3eedd799c720b296157724cfe
VijetHegde604 <vijethegde604@gmail.com>
2026-01-31 12:55:41 +0530

    corrected the way of handling flake updates

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -22,7 +22,7 @@
       nrt = "sudo nixos-rebuild test --flake ~/nixos-config#nix-btw";
 
       # update flake inputs then rebuild
-      nfu = "nix flake update ~/nixos-config && sudo nixos-rebuild switch --flake ~/nixos-config#nix-btw";
+      nfu = "nix flake update --flake ~/nixos-config && sudo nixos-rebuild switch --flake ~/nixos-config#nix-btw";
 
       # home-manager only rebuild (through nixos module)
       hms = "home-manager switch --flake ~/nixos-config";
