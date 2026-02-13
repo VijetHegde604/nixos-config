4b809b5f5ae4d7a5986d5751b0c182e1cf1a5710
VijetHegde604 <vijethegde604@gmail.com>
2026-02-13 13:29:40 +0530

    feat: Added zram swap

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -6,8 +6,15 @@
   services.udisks2.enable = true;
   services.dbus.enable = true;
 
-  services.gvfs.enable = true;   # REQUIRED for trash, mtp, smb, auto-mount
-  services.tumbler.enable = true; # thumbnails 
+  services.gvfs.enable = true; # REQUIRED for trash, mtp, smb, auto-mount
+  services.tumbler.enable = true; # thumbnails
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd"; # best default
+    memoryPercent = 50; # 25–50% is ideal
+    priority = 100; # higher = preferred over disk swap
+  };
 
   system.autoUpgrade.enable = true;
   system.autoUpgrade.dates = "weekly";
