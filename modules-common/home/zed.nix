5ab71dc920c65b656c0e84b739110edb02a60c35
VijetHegde604 <vijethegde604@gmail.com>
2026-03-01 15:47:59 +0530

    update: Added config for zed with updated options and lsp

--- modules-common/home/zed.nix
+++ modules-common/home/zed.nix
@@ -1,21 +1,76 @@
 { ... }:
+
 {
   programs.zed-editor = {
     enable = true;
+    # Automatically install extensions via Nix
     extensions = [
       "nix"
-      "toml"
       "rust"
+      "toml"
+      "go"
+      "python"
+      "yaml"
+      "json"
     ];
+
     userSettings = {
+      telemetry = {
+        diagnostics = false;
+        metrics = false;
+      };
+      autosave = {
+        after_delay = {
+          milliseconds = 500;
+        };
+      };
       theme = {
         mode = "dark";
         dark = "Ayu Dark";
         light = "Ayu Light";
       };
-      autosave = {
-        after_delay = {
-          milliseconds = 500;
+
+      ui_font_size = 16;
+      buffer_font_size = 15;
+      buffer_font_family = "JetBrains Mono"; # Highly recommended for coding
+
+      terminal = {
+        font_family = "JetBrains Mono";
+        copy_on_select = true;
+      };
+
+      languages = {
+        Go = {
+          format_on_save = "on";
+          formatter = "auto";
+        };
+        Python = {
+          format_on_save = "on";
+          formatter = "auto";
+        };
+        JavaScript = {
+          format_on_save = "on";
+          formatter = {
+            external = {
+              command = "auto";
+              arguments = [
+                "--stdin-filepath"
+                "{buffer_path}"
+              ];
+            };
+          };
+        };
+      };
+
+      lsp = {
+        gopls = {
+          settings = {
+            hints = {
+              assignVariableTypes = true;
+              compositeLiteralFields = true;
+              parameterNames = true;
+            };
+          };
         };
       };
     };
