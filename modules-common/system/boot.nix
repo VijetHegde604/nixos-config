6a0adf076ef2e31abda9d3db0d451fdc06b82d14
VijetHegde604 <vijethegde604@gmail.com>
2026-03-03 23:44:53 +0530

    feat: Added plymouth with silent boot

--- modules-common/system/boot.nix
+++ modules-common/system/boot.nix
@@ -4,4 +4,14 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  plymouth.enable = true;
+  # Enable "Silent boot"
+  consoleLogLevel = 3;
+  initrd.verbose = false;
+  kernelParams = [
+    "quiet"
+    "udev.log_level=3"
+    "systemd.show_status=auto"
+  ];
 }
