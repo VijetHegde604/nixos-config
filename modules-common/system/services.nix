807e0260c3eef3c3c090d8835f4b1638e53b4ef6
VijetHegde604 <vijethegde604@gmail.com>
2026-03-09 03:11:31 +0530

    feat: Added accounts-daemon to persist changes like profile pictures in DMS

--- modules-common/system/services.nix
+++ modules-common/system/services.nix
@@ -5,6 +5,7 @@
 
   services.udisks2.enable = true;
   services.dbus.enable = true;
+  services.accounts-daemon.enable = true;
 
   services.gvfs.enable = true; # REQUIRED for trash, mtp, smb, auto-mount
   services.tumbler.enable = true; # thumbnails
