fe4a31bd52b6d5d0087d3297ef69b9b33e90e844
VijetHegde604 <vijethegde604@gmail.com>
2026-03-10 17:19:58 +0530

    update: Added the server monitoring script to the systemd service config

--- hosts/nix-server/modules/users.nix
+++ hosts/nix-server/modules/users.nix
@@ -18,4 +18,27 @@
     HandleLidSwitchDocked = "ignore";
   };
 
+  systemd.services.sys-monitor-api = {
+    description = "Python System Monitoring API";
+
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      User = "vijeth";
+      WorkingDirectory = "/home/vijeth/system-monitor-api";
+
+      ExecStart = "/home/vijeth/system-monitor-api/.venv/bin/python /home/vijeth/system-monitor-api/system-monitor.py";
+
+      Restart = "always";
+      RestartSec = 5;
+
+      NoNewPrivileges = true;
+      PrivateTmp = true;
+    };
+  };
+  };
+
 }
