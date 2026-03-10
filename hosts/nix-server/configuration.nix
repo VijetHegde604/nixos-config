a3a090190380971bb14a4f4243a430c507fc7b41
VijetHegde604 <vijethegde604@gmail.com>
2026-03-10 11:00:14 +0530

    update configuration.nix

--- hosts/nix-server/configuration.nix
+++ hosts/nix-server/configuration.nix
@@ -17,11 +17,25 @@
   # Latest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # Hostname
-  networking.hostName = "nix-server";
-
   # Networking
-  networking.networkmanager.enable = true;
+  networking = {
+  hostName = "nix-server";
+
+  networkmanager.enable = false;
+
+  interfaces.enp2s0.useDHCP = false;
+
+  interfaces.enp2s0.ipv4.addresses = [
+    {
+      address = "192.168.1.70";
+      prefixLength = 24;
+    }
+  ];
+
+  defaultGateway.address = "192.168.1.1";
+
+  nameservers = [ "192.168.1.69" "1.1.1.1" ];
+  };
 
   # Timezone
   time.timeZone = "Asia/Kolkata";
