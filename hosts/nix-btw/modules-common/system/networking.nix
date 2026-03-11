976eccfe3744f1af81121a1254b8093c6275be16
VijetHegde604 <vijethegde604@gmail.com>
2026-03-11 23:07:03 +0530

    update: Made pihole to be the primary DNS and added cloudflare as a fallback

--- hosts/nix-btw/modules-common/system/networking.nix
+++ hosts/nix-btw/modules-common/system/networking.nix
@@ -3,8 +3,12 @@
 {
   networking.hostName = "nix-btw";
   networking.networkmanager.enable = true;
-  networking.nameservers = [ "100.95.4.126" "1.1.1.1" ];
 
-  services.resolved.enable = true;
+  networking.nameservers = [ "100.95.4.126" ];
+  services.resolved = {
+    enable = true;
+    fallbackDns = [ "1.1.1.1" ];
+  };
+
   services.tailscale.enable = true;
 }
