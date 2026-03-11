5120e97b8bc9857c02fbca34c53dd02920c77f7f
VijetHegde604 <vijethegde604@gmail.com>
2026-03-11 23:19:58 +0530

    update: Refactored the resolved settings

--- hosts/nix-btw/modules-common/system/networking.nix
+++ hosts/nix-btw/modules-common/system/networking.nix
@@ -7,7 +7,11 @@
   networking.nameservers = [ "100.95.4.126" ];
   services.resolved = {
     enable = true;
-    fallbackDns = [ "1.1.1.1" ];
+    settings = {
+      Resolve = {
+        FallbackDns = [ "1.1.1.1" ];
+      };
+    };
   };
 
   services.tailscale.enable = true;
