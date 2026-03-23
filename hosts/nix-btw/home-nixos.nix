f195975e2b4f4377772449c73270eb27d6d077cf
VijetHegde604 <vijethegde604@gmail.com>
2026-03-23 15:04:01 +0530

    update: stateVersion to 26.05

--- hosts/nix-btw/home-nixos.nix
+++ hosts/nix-btw/home-nixos.nix
@@ -17,7 +17,7 @@
 
   home.username = settings.username;
   home.homeDirectory = "/home/${settings.username}";
-  home.stateVersion = "25.11";
+  home.stateVersion = "26.05";
 
   programs.home-manager.enable = true;
 }
