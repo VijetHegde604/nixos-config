22f164d495741193534221bd76f19f6a66cfb7f3
VijetHegde604 <vijethegde604@gmail.com>
2026-03-27 00:58:20 +0530

    update: Added graphics setting for intel-iris-Xe

--- hosts/nix-btw/modules-common/system/hardware.nix
+++ hosts/nix-btw/modules-common/system/hardware.nix
@@ -11,6 +11,28 @@
     powerOnBoot = true;
   };
 
+  # ------- Graphics Settings -------- #
+
+  services.xserver.videoDrivers = [ "modesetting" ];
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vpl-gpu-rt
+      intel-compute-runtime
+    ];
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+
+  hardware.enableRedistributableFirmware = true;
+  boot.kernelParams = [ "i915.enable_guc=3" ];
+
+  # -------# -------# -------# -------# -------# -------# -------# -------# -------# -------#
+
   services.libinput.enable = true;
   services.upower.enable = true;
   services.acpid.enable = true;
