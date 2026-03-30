fa9f2697cc0906693e357c17ca9035ea84284220
VijetHegde604 <vijethegde604@gmail.com>
2026-03-30 15:44:35 +0530

    Update virtualization.nix

--- hosts/nix-btw/modules-common/system/virtualization.nix
+++ hosts/nix-btw/modules-common/system/virtualization.nix
@@ -1,8 +1,34 @@
-{ settings, ... }:
+{ pkgs, settings, ... }:
 
 {
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  virtualisation.virtualbox.guest.dragAndDrop = true;
-  users.extraGroups.vboxusers.members = [ settings.username ];
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true; # Required for Windows 11 VMs (TPM support)
+    };
+  };
+
+  users.users.${settings.username}.extraGroups = [
+    "libvirtd"
+    "kvm"
+  ];
+
+  # 3. Essential packages for managing VMs
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    spice
+    spice-gtk
+    spice-protocol
+    virtio-win
+    virtiofsd
+  ];
+
+  # 4. Networking: Enable the bridge interface
+  networking.firewall.trustedInterfaces = [ "virbr0" ];
+
+  # 5. Enable SPICE for copy-paste and file sharing between host and guest
+  services.spice-vdagentd.enable = true;
 }
