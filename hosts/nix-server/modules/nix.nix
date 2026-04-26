ae57c15c546749a678a7f270b6a112b37f31b093
VijetHegde604 <vijethegde604@gmail.com>
2026-04-26 13:33:33 +0530

    chore: improve GC retention and stabilize auto-upgrades

--- hosts/nix-server/modules/nix.nix
+++ hosts/nix-server/modules/nix.nix
@@ -14,18 +14,22 @@
     gc = {
       automatic = true;
       dates = "daily";
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 14d";
     };
   };
 
+  # Enable nh (Nix Helper)
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 14d --keep 3";
+    flake = "/home/vijeth/nixos-config";
+  };
+
   # # Keep upgrades automatic but deterministic through pinned flake.lock.
-  # system.autoUpgrade = {
-  #   enable = true;
-  #   dates = "weekly";
-  #   flake = "github:VijetHegde604/nixos-config";
-  #   flags = [
-  #     "--update-input"
-  #     "nixpkgs"
-  #   ];
-  # };
+  system.autoUpgrade = {
+    enable = true;
+    dates = "weekly";
+    flake = "github:VijetHegde604/nixos-config";
+  };
 }
