5913ada85389e9f10630fc74fc7e48cbf235cc5f
VijetHegde604 <vijethegde604@gmail.com>
2026-05-01 23:06:06 +0530

    update: Added optional gaming and optimized steam for niri fixed steam not rendering

--- hosts/nix-btw/configuration.nix
+++ hosts/nix-btw/configuration.nix
@@ -20,7 +20,7 @@
     #./modules/system/syncthing.nix
   ]
   ++ lib.optional settings.virtualization ./modules/system/virtualization.nix
-  ++ lib.optional.settings.gaming ./modules/system/steam.nix;
+  ++ lib.optional settings.gaming ./modules/system/steam.nix;
 
   users.users.vijeth = {
     isNormalUser = true;
--- hosts/nix-btw/modules/system/steam.nix
+++ hosts/nix-btw/modules/system/steam.nix
@@ -1,6 +1,9 @@
-{ ... }:
+{ pkgs }:
 {
   programs.steam = {
     enable = true;
+    package = pkgs.steam.override {
+      extraArgs = "-system-composer";
+    };
   };
 }
