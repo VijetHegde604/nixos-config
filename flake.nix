9ed3efd56b6eb0b683487044756f6ef1915ae673
VijetHegde604 <vijethegde604@gmail.com>
2026-06-03 22:22:18 +0530

    reverted home-manager version to unstable

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
