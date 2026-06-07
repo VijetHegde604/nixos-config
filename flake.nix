09562a11798d9bf27d8d9371b783317095c183e4
VijetHegde604 <vijethegde604@gmail.com>
2026-06-07 23:46:48 +0530

    update: Added extra substitutes for chaotic nyx cache

--- flake.nix
+++ flake.nix
@@ -7,11 +7,13 @@
       "https://cache.garnix.io"
       "https://attic.xuyh0120.win/lantian"
       "https://vicinae.cachix.org"
+      "https://nyx-cache.chaotic.cx/"
     ];
     extra-trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+      "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk="
     ];
   };
 
