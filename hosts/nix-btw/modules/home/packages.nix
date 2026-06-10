40fe37f9440a78ae1188093ab4116dbb462b0234
VijetHegde604 <vijethegde604@gmail.com>
2026-06-10 23:45:26 +0530

    opened ports for kde connect

--- hosts/nix-btw/modules/home/packages.nix
+++ hosts/nix-btw/modules/home/packages.nix
@@ -35,8 +35,4 @@
 
   # KDE Connect 
   services.kdeconnect.enable = true;
-  networking.firewall = rec {
-    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
-    allowedUDPPortRanges = allowedTCPPortRanges;
-  };
 }
--- hosts/nix-btw/modules/system/networking.nix
+++ hosts/nix-btw/modules/system/networking.nix
@@ -4,6 +4,17 @@
   networking.hostName = settings.hostname;
   networking.networkmanager.enable = true;
 
+  # opened ports for KDE Connect
+  networking.firewall = rec {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
+
   networking.nameservers = [ "100.95.4.126" ];
   services.resolved = {
     enable = true;
