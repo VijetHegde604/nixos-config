8f84a665dffc6be16901b30e3654b9b5c06d3e17
VijetHegde604 <vijethegde604@gmail.com>
2026-06-24 23:31:57 +0530

    added immich as a native nixos module

---
+++ hosts/nix-server/modules/immich.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+
+  services.immich = {
+    enable = true;
+    port = 2284;
+    host = "0.0.0.0";
+    openFirewall = true;
+    mediaLocation = "~/immich";
+    accelerationDevices = null;
+  };
+}
