5946695d890c3cec10997ebc31c0345028262eca
VijetHegde604 <vijethegde604@gmail.com>
2026-06-25 00:12:13 +0530

    removed immich for now

--- hosts/nix-server/modules/immich.nix
+++ hosts/nix-server/modules/immich.nix
@@ -1,12 +1,12 @@
-{ ... }:
-{
+# { ... }:
+# {
 
-  services.immich = {
-    enable = true;
-    port = 2284;
-    host = "0.0.0.0";
-    openFirewall = true;
-    mediaLocation = "/media/immich";
-    accelerationDevices = null;
-  };
-}
+#   services.immich = {
+#     enable = true;
+#     port = 2284;
+#     host = "0.0.0.0";
+#     openFirewall = true;
+#     mediaLocation = "/media/immich";
+#     accelerationDevices = null;
+#   };
+# }
