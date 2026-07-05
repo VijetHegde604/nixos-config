9688e4d262f6ad29f027dc0abaad5073017dc284
VijetHegde604 <vijethegde604@gmail.com>
2026-07-05 16:46:12 +0530

    removed for now

--- hosts/nix-server/modules/ollama.nix
+++ hosts/nix-server/modules/ollama.nix
@@ -1,14 +1,14 @@
 { pkgs, ...}:
 {
-  services.ollama = {
-    enable = true;
-    # Uses the CUDA-accelerated package to offload to the MX330
-    package = pkgs.ollama-cuda;
-
-    # Optional: Declaratively pull models on startup
-    loadModels = [
-      "llama3.2:3b"
-      "qwen2.5-coder:7b"
-    ];
-  };
+#  services.ollama = {
+#    enable = true;
+#    # Uses the CUDA-accelerated package to offload to the MX330
+#    package = pkgs.ollama-cuda;
+#
+#    # Optional: Declaratively pull models on startup
+#    loadModels = [
+#      "llama3.2:3b"
+#      "qwen2.5-coder:7b"
+#    ];
+#  };
 }
