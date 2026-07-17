5dce1f8792b4595db42e8c2186590f18ce122674
VijetHegde604 <vijethegde604@gmail.com>
2026-07-17 14:42:51 +0530

    added fcitx autocomplete engine

--- hosts/nix-btw/modules/system/locale.nix
+++ hosts/nix-btw/modules/system/locale.nix
@@ -1,6 +1,12 @@
-{ settings, ... }:
+{ settings,pkgs, ... }:
 
 {
   time.timeZone = settings.timezone;
   i18n.defaultLocale = settings.locale;
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-mozc fcitx5-gtk];
+  };
 }
