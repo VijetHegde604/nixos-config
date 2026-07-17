af58155d31ef8eba633767672b0b01a739ffffca
VijetHegde604 <vijethegde604@gmail.com>
2026-07-17 14:50:37 +0530

    removed autocomplete

--- hosts/nix-btw/modules/system/locale.nix
+++ hosts/nix-btw/modules/system/locale.nix
@@ -1,12 +1,6 @@
-{ settings,pkgs, ... }:
+{ settings, ... }:
 
 {
   time.timeZone = settings.timezone;
   i18n.defaultLocale = settings.locale;
-
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-mozc fcitx5-gtk];
-  };
 }
